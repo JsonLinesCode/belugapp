7acbe5edb39adf69f05ba975f8a5547efab73fe1
ModernChocolate <modernchocolate@users.noreply.github.com>
2021-05-12 14:47:25 +0200

    :heavy_plus_sign: Add googleapis dependency and start scenario

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const clear = require('clear');
 const figlet = require('figlet');
 const inquirer  = require('./lib/inquirer');
 clear();
+
 console.log(
   chalk.blueBright(
     figlet.textSync('BelugApp', { horizontalLayout: 'full' })
   )
 );
-inquirer.askFirstChoice();
+
+var choice = inquirer.askFirstChoice();
+
