3eac3b385d233a41dc5532f514e8934b7092d5fd
ModernChocolate <modernchocolate@users.noreply.github.com>
2021-05-13 11:42:13 +0200

    :sparkles: Add a table generator

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const inquirer  = require('./lib/inquirer');
-const scenario  = require('./lib/scenario');
+const inquirer = require('./lib/inquirer');
+const scenario = require('./lib/scenario');
 
 clear();
-
-console.log(
-  chalk.blueBright(
-    figlet.textSync('BelugApp', { horizontalLayout: 'full' })
-  )
-);
-
-var choice = inquirer.askFirstChoice().then((e)=>
-{
+console.log("Welcome to belugapp. This is a really simple API to check your apps status on PlayStore and AppStore.\nPlease check our configuration guide before using it.\n")
+var choice = inquirer.askFirstChoice().then((e) => {
   scenario.launch(e["firstChoice"])
 });
